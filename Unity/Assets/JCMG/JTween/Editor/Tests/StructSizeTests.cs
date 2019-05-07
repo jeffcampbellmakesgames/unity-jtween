ba2713ffe09883907e715d9de87e33a30dce41a1
Jeff Campbell <jeff@zapdot.com>
2019-05-07 09:00:29 +0200

    Added tests to ensure struct size is LTE 40 bytes
    * Added tests to ensure struct size is less than or equal to 40 bytes; based on profiling this seems to prevent str.memcpy allocations on Mono.

---
+++ Unity/Assets/JCMG/JTween/Editor/Tests/StructSizeTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace JCMG.JTween.Editor.Tests
+{
+	public class StructSizeTests
+	{
+		[Test]
+		public void StructSizeTestsSimplePasses()
+		{
+			// When structs were being copied to and from native I had seen memcpy.string calls that caused
+			// allocations, but did not when they were 40 or less bytes. From research this appears to be a
+			// Mono issue.
+			Assert.LessOrEqual(TweenTransformState.SizeOf(), 40, "TweenState Size: {0} > 40", TweenTransformState.SizeOf());
+			Assert.LessOrEqual(TweenLifetime.SizeOf(), 40, "TweenLifetime Size: {0} > 40", TweenLifetime.SizeOf());
+			Assert.LessOrEqual(TweenPosition.SizeOf(), 40, "TweenPosition Size: {0} > 40", TweenPosition.SizeOf());
+			Assert.LessOrEqual(TweenRotation.SizeOf(), 40, "TweenRotation Size: {0} > 40", TweenRotation.SizeOf());
+			Assert.LessOrEqual(TweenScale.SizeOf(), 40, "TweenScale Size: {0} > 40", TweenScale.SizeOf());
+		}
+	}
+}
